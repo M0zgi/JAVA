d779a23f77dfd7f3810ade05cb9773c0bcbaa99d
Aleksandr <mozg.com@gmail.com>
2023-03-22 17:48:30 +0100

    Task1

---
+++ HW1/HW1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+/*        Задание 1
+        Выведите на экран надпись "Your time is limited, so
+        don’t waste it living someone else’s life" Steve Jobs на разных
+        строках. Пример вывода:
+        “Your time is limited,
+          so don’t waste it
+              living someone else’s life”
+                   Steve Jobs
+
+ */
+        System.out.println("""
+                Your time is limited,\s
+                \tso don’t waste it\s
+                \t\tliving someone else’s life\s
+                \t\t\tSteve Jobs""");
+    }
+}
