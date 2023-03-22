2b019ca2aef35e2479820a07588b32b36448cd78
Aleksandr <mozg.com@gmail.com>
2023-03-22 18:03:12 +0100

    Task2

--- HW1/HW1/src/Main.java
+++ HW1/HW1/src/Main.java
@@ -1,20 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-/*        Задание 1
-        Выведите на экран надпись "Your time is limited, so
-        don’t waste it living someone else’s life" Steve Jobs на разных
-        строках. Пример вывода:
+    /*  Задание 1
+        Выведите на экран надпись "Your time is limited, so don’t waste it living someone else’s life" Steve Jobs
+        на разных строках. Пример вывода:
         “Your time is limited,
           so don’t waste it
               living someone else’s life”
                    Steve Jobs
 
- */
-        System.out.println("""
-                Your time is limited,\s
-                \tso don’t waste it\s
-                \t\tliving someone else’s life\s
-                \t\t\tSteve Jobs""");
+    */
+        {
+            System.out.println("""
+                    Your time is limited,\s
+                    \tso don’t waste it\s
+                    \t\tliving someone else’s life\s
+                    \t\t\tSteve Jobs""");
+        }
+
+    /*  Задание 2
+        Пользователь вводит с клавиатуры два числа. Первое
+        число — это значение, второе число процент, который
+        необходимо посчитать. Например, мы ввели с клавиатуры
+        50 и 10. Требуется вывести на экран 10 процентов от 50.
+        Результат: 5
+    */
+
+        {
+            int number, percent;
+            Scanner inputNumScanner = new Scanner(System.in);
+
+            System.out.println("Enter number: ");
+            number = inputNumScanner.nextInt();
+
+            System.out.println("Enter %: ");
+            percent = inputNumScanner.nextInt();
+
+            float result = (number * percent / 100);
+
+            System.out.println(percent + " % from " + number + " = " + result);
+        }
     }
 }
